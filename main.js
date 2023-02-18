01bad375c8e8ba995d83102df0aee5ed24b14782
yuvancoding <112766393+yuvancoding@users.noreply.github.com>
2023-02-18 19:48:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function(event) {
+    console.log(event);
+
+    var Content =event.results[0][0].transcript;
+
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+    speak();
+}
+//var SpeechRecognition = window.webkitSpeechRecognition;
+
+//var recognition = new SpeechRecognition();
+
+function speak() {
+    var synth = window.speechSynthesis;
+
+    speak_data = document.getElementById("textbox").innerHTML;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+ }
+
+ camera = document.getElementById("camera");
+ Webcam.set({
+    width: 360,
+    height: 250,
+   image_format : 'jpeg',
+   jpeg_quality:90
+ });
